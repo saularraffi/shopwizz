0c0fc55e01bc91a0fd4546e4dddd154ab9ad4abb
Saular Raffi <saular.raffi@yahoo.com>
2024-01-16 17:47:44 -0500

    send test set from backend and create frontent table to view apps and sort by rating

--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [
-    path('', views.getData),
-    path('test', views.getTest),
-    path('test2', views.getTest2),
-    path('urls', views.getUrls),
-]
