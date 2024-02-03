ef28290be9cf8b51769f5209434dde5dc1ef0166
Saular Raffi <saular.raffi@yahoo.com>
2024-02-03 15:47:48 -0500

    add logo to page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import AppsTable from "./AppsTable";
+import { Container } from "@mui/material";
 
 function App() {
     return (
         <>
+            <Container sx={{ marginBottom: "20px" }}>
+                <img
+                    style={{ width: "350px" }}
+                    src="/shopwizz_logo_full.png"
+                ></img>
+            </Container>
             <AppsTable />
         </>
     );
--- frontend/src/appsTable.jsx
+++ frontend/src/appsTable.jsx
@@ -164,7 +164,7 @@ export default function AppsTable() {
     ]);
 
     return (
-        <Container sx={{ marginBottom: "100px", marginTop: "50px" }}>
+        <Container sx={{ marginBottom: "100px" }}>
             <Box
                 sx={{
                     minWidth: 200,
